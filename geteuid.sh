2145121b2907ab75ce453d1dd59301df2f9f1dfc
Chad Carr <ccarr@franzdoodle.com>
2019-03-09 21:56:34 -0500

    Commits to main module and submodule

--- geteuid.sh
+++ geteuid.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# This script might be passed to rexec() for remote execution.
 
-. ./common/base.sh
+# source ./common/base.sh # Cannot source local code in rexec scripts
+# (But network.sh exports useful constants and functions from local libraries)
+
+loglevel=$DEBUG
 
 debug "euid = $(id -u)"
+info "host = $(hostname), id = $(id)"
+warn "euid = $(id -u)"
+err "euid = $(id -u)"
--- scratch.sh
+++ scratch.sh
@@ -1,62 +1,32 @@
 #!/bin/bash
+# This type of script would be called by a Jenkins freestyle project
+# using . ./scratch.sh
 
-suppOS="Liux"
-SSH="ssh -l vagrant"
+. ./common/base.sh
+. ./common/network.sh
 
+loglevel=$DEBUG
+
+ssh+=" -l vagrant"
 for k in .vagrant/machines/*/virtualbox/private_key; do
-    SSH+=" -i $k"
+    ssh+=" -i $k"
 done
 
-rexec() {
-    local user=root
-    local host=$1 # default to bare host - user and port are optional
-    local port=22
+suppOS="Linux"
 
-    # Host may be plain host or ip, user@host, host:port
-    # or the trifecta: user@host:port
-
-    # user may precede @ 
-    [[ $1 =~ ^(.*)@ ]] && {
-        user=${BASH_REMATCH[1]}
-    }
-
-    # host may follow an @ or precede a :
-    [[ $1 =~ @([^@:]*) ]] && {
-        host=${BASH_REMATCH[1]}
-    }
-    [[ $1 =~ ([^@:]*): ]] && {
-        host=${BASH_REMATCH[1]}
-    }
-
-    # port may follow a :
-    [[ $1 =~ :(.*)$ ]] && {
-        port=${BASH_REMATCH[1]};
-    }
-
-    local code=$2 # single-quoted code to be executed remotely
-    shift 2 # shift off the first two args...
-    local refs=("$@") # the rest are namerefs for remote code
-
-    {
-        declare -p "${refs[@]}"
-        echo "$code"
-    }# | $SSH -p $port $host sudo -u $user "bash -s"
-}
-
-declare -A hosts=(
+declare -A roles=(
     [control]='localhost:2222'
-    [work1]='127.0.0.1:2200'
+    [work1]='vagrant@127.0.0.1:2200'
     [work2]='127.0.0.1:2201'
 )
 
-for host in "${!hosts[@]}"; do
-    echo "Remote exec on $host"
-    output=$(rexec ${hosts[$host]} '
-        id
-        hostname
+for role in "${!roles[@]}"; do
+    info "remote exec on $role"
+    output=$(rexec ${roles[$role]} '
+        df -h
         hostOS=$(uname -s)
-        [[ $hostOS == $suppOS ]] || echo "Error: running on $hostOS not supported, this script supports $suppOS" >&2
+        [[ $hostOS == $suppOS ]] || err "running on $hostOS not supported, this script supports $suppOS"
     ' suppOS)
     echo "$output"
-    rexec ${hosts[$host]} geteuid.sh
+    rexec ${roles[$role]} geteuid.sh
 done
